50cb5911e3b498872d5d240e427bdcb5c166ceae
Prudvi Gadeshula <118651402+Prudvi14@users.noreply.github.com>
2025-08-18 21:54:05 +0530

    Update compound_interest.py

--- compound_interest.py
+++ compound_interest.py
@@ -1,12 +1,12 @@
 # This script calculates yearly compound interest given principal, annual rate of interest and time period in years.
 # Do not use this in production. Sample purpose only.
 
-# Author: Upkar Lidder (IBM)
+# Author: prudvi (IBM)
 
 # Input:
-# p, principal amount
-# t, time period in years
-# r, annual rate of interest
+# p= principal amount
+# t= time period in years
+# r= annual rate of interest
 
 # Output:
 # compound interest = p * (1 + r/100)^t
